f047f5af83ddf12a956525c451deab1edb621be5
Sarina <joshisarina19@gmail.com>
2026-05-22 12:56:46 +0545

    backend

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const multer = require("multer")
+const uploadFile = require("./services/storage.service");
+const postModel = require("./models/post.model.js");
+
+const app = express()
+app.use(express.json()); //raw 
+const upload = multer({storage: multer.memoryStorage()}) //file 
+
+
+
+app.post("/create-post", upload.single("image") ,async (req,res)=>{
+
+    const result = await uploadFile(req.file.buffer)
+     
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+    return res.status(201).json({
+        message: "Post created successfully",
+        post
+    })
+})
+app.get("/posts", async (req,res)=>{
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        message:"posts fetched successfully",
+        posts
+    })
+
+})
+
+
+module.exports = app;
